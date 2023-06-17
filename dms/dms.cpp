4a23bbc30f9d8a76930e21b163194567c41f5502
naivete13sky <chen320821@163.com>
2023-06-17 16:53:07 +0800

    17-1 ok,QNetworkAccessManager post成功不会自动打开要跳转的链接的，这个和python的requests是不一样的。得自已再写打开的逻辑。

--- dms/dms.cpp
+++ dms/dms.cpp
@@ -52,31 +52,41 @@ json DMS::login(const QString username, const QString password) {
 
 
             // 创建 QNetworkRequest 对象并设置 URL
-            QNetworkRequest request(QUrl("http://10.97.80.119/admin/login/"));
+            QNetworkRequest request2(QUrl("http://10.97.80.119/admin/login/"));
 
             // 设置请求头
-            request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
-
+            request2.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+            request2.setRawHeader("User-Agent", "python-requests/2.30.0");
+            request2.setRawHeader("Accept", "*/*");
             // 构建 POST 请求的数据
-            QString postData = QString("csrfmiddlewaretoken=%1&username=cc&password=cc&next=%2Fadmin%2F").arg(QString::fromStdString(csrfmiddlewaretoken));
-            // 这里的 "your_username" 和 "your_password" 是您要提交的实际用户名和密码
+            //QString postData = QString("csrfmiddlewaretoken=%1&username=cc&password=cc&next=%2Fadmin%2F").arg(QString::fromStdString(csrfmiddlewaretoken));
+            
 
-
-            // 构建 POST 数据            
-            postData1.addQueryItem("csrfmiddlewaretoken", QString::fromStdString(csrfmiddlewaretoken));
-            postData1.addQueryItem("username", "cc");
-            postData1.addQueryItem("password", "cc");
-            postData1.addQueryItem("next", "%2Fadmin%2F");
+            QByteArray postData;
+            postData.append("csrfmiddlewaretoken=");
+            postData.append(csrfmiddlewaretoken.c_str());
+            postData.append("&username=cc&password=cc&next=%2Fadmin%2F");
+            QNetworkReply* reply2 = manager.post(request2, postData);
+            QEventLoop loop2;
+            QObject::connect(reply2, &QNetworkReply::finished, &loop2, &QEventLoop::quit);
+            loop2.exec();
+            
+            
+            //// 构建 POST 数据            
+            //postData1.addQueryItem("csrfmiddlewaretoken", QString::fromStdString(csrfmiddlewaretoken));
+            //postData1.addQueryItem("username", "cc");
+            //postData1.addQueryItem("password", "cc");
+            //postData1.addQueryItem("next", "%2Fadmin%2F");
             // 发送 POST 请求，模拟登录            
-            QNetworkRequest postRequest(QUrl("http://10.97.80.119/admin/login/"));
-            postRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+            //QNetworkRequest postRequest(QUrl("http://10.97.80.119/admin/login/"));
+            //postRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
-            reply = manager.post(postRequest, postData1.toString(QUrl::FullyEncoded).toUtf8());
-            QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
-            loop.exec();
+            //reply = manager.post(postRequest, postData1.toString(QUrl::FullyEncoded).toUtf8());
+            //QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+            //loop.exec();
 
             // 处理登录后的返回结果
-            data = reply->readAll();
+            //data = reply->readAll();
             
 
             // 发送 POST 请求
@@ -89,10 +99,10 @@ json DMS::login(const QString username, const QString password) {
 
             //loop.exec();
 
-            if (reply->error() == QNetworkReply::NoError) {
+            if (reply2->error() == QNetworkReply::NoError) {
                 
-                qDebug() << "reply2:" << reply;
-                QByteArray data2 = reply->readAll();
+                qDebug() << "reply2:" << reply2;
+                QByteArray data2 = reply2->readAll();
                 // 处理响应数据
                 QTextCodec* codec2 = QTextCodec::codecForName("UTF-8");
                 QString decodedData2 = codec2->toUnicode(data2);
@@ -100,6 +110,24 @@ json DMS::login(const QString username, const QString password) {
                 qDebug().noquote() << "decodedData2:" << decodedData2;
 
             }
+
+            reply2->deleteLater();
+
+
+
+
+            QNetworkReply* reply3 = manager.get(QNetworkRequest(QUrl("http://10.97.80.119/admin/")));
+            QEventLoop loop3;
+
+            QObject::connect(reply3, &QNetworkReply::finished, &loop3, &QEventLoop::quit);
+
+            loop3.exec();
+            QByteArray data3 = reply3->readAll();
+            // 处理响应数据
+            QTextCodec* codec3 = QTextCodec::codecForName("UTF-8");
+            QString decodedData3 = codec3->toUnicode(data3);
+            qDebug().noquote() << "decodedData3:" << decodedData3;
+
 
 
         }
@@ -124,6 +152,7 @@ json DMS::login(const QString username, const QString password) {
     }
 
     reply->deleteLater();
+    
 
     return jsonData;
 }
